440dd2388a42fa71c30e543d87224f379e116e1d
Dave Pearson <davep@davep.org>
2018-04-23 09:35:03 +0100

    :bookmark: v1.01

--- commoji.el
+++ commoji.el
@@ -2,7 +2,7 @@
 ;; Copyright 2018 by Dave Pearson <davep@davep.org>
 
 ;; Author: Dave Pearson <davep@davep.org>
-;; Version: 1.00
+;; Version: 1.01
 ;; Keywords: convenience
 ;; URL: https://github.com/davep/commoji.el
 ;; Package-Requires: ((emacs "24"))
